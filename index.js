b2bb05e228009cd5bf05bcffe1809375bb65f257
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-25 13:23:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+function driversWithRevenueOver(drivers, revenue){
+
+return drivers.filter(function (driver){
+	if( driver.revenue > revenue){
+		return driver;
+	}
+});
+}
+
+function driverNamesWithRevenueOver(drivers, revenue){
+ return driversWithRevenueOver(drivers,revenue).map(function(eachDriver){
+ 		 return eachDriver.name;
+ 
+ 	});
+
+}
+function exactMatch(drivers, attr){
+return drivers.filter(function(driver){
+ for (const key in attr){
+ 	// debugger;
+ 	if (driver[key] === attr[key]){
+ 		return driver;
+ 	}
+ 	}		
+ 	})
+}
+
+function exactMatchToList(drivers,attr){
+	return exactMatch(drivers,attr).map(function(driver){
+	return	driver.name;
+	});
+}
+
+
 // Code your solution here:
